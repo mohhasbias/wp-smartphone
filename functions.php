5eb1d4e55017a4f02e7f86d2fb39d8467f062941
Moh Hasbi Assidiqi <mohhasbias@gmail.com>
2014-02-15 22:18:44 +0700

    connect topbar to wordpress system

--- functions.php
+++ functions.php
@@ -658,4 +658,35 @@ function r_wp_html_excerpt( $str, $count, $more = null ) {
   return $excerpt;
 }
 
+// topbar walker, thanks to gareth cooper (http://garethcooper.com/2014/01/zurb-foundation-5-and-wordpress-menus/)
+class GC_walker_nav_menu extends Walker_Nav_Menu {
+ 
+  // add classes to ul sub-menus
+  function start_lvl(&$output, $depth) {
+    // depth dependent classes
+    $indent = ( $depth > 0 ? str_repeat("\t", $depth) : '' ); // code indent
+ 
+    // build html
+    $output .= "\n" . $indent . '<ul class="dropdown">' . "\n";
+  }
+}
+
+if (!function_exists('GC_menu_set_dropdown')) :
+function GC_menu_set_dropdown($sorted_menu_items, $args) {
+  $last_top = 0;
+  foreach ($sorted_menu_items as $key => $obj) {
+    // it is a top lv item?
+    if (0 == $obj->menu_item_parent) {
+      // set the key of the parent
+      $last_top = $key;
+    } else {
+      $sorted_menu_items[$last_top]->classes['dropdown'] = 'has-dropdown';
+    }
+  }
+
+  return $sorted_menu_items;
+}
+endif;
+add_filter('wp_nav_menu_objects', 'GC_menu_set_dropdown', 10, 2);
+
 ?>
