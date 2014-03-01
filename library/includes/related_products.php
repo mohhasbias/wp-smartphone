762522e884c757d00edce40559de1d18c3cedc11
Moh Hasbi Assidiqi <mohhasbias@gmail.com>
2014-03-01 09:18:45 +0700

    refine related product style

--- library/includes/related_products.php
+++ library/includes/related_products.php
@@ -2,13 +2,13 @@
 global $General;
 $post_array = $General->get_post_array($post->ID);
 ?>
-<div class="realated_product_section clearfix row">
-	<div class="ten columns">
+<div class="realated_product_section clearfix">
+	<div class="small-12 columns">
 	  <div class="options">
 	    <h3><?php _e('Related Products');?></h3>
 	  </div>
 	</div>
-  <div class="realated_products small-12 columns">
+  <div class="products_box small-12 columns">
 <?php
 if($post_array)
 {
@@ -23,21 +23,27 @@ if($post_array)
 			$imagepath = WP_CONTENT_DIR.str_replace(get_option( 'siteurl' ).'/wp-content','',$image_array[0]);
 			$relatedprd_count++; 
 ?>
-			<div class="small-12 columns">
-	    	<div class="content_block text-center thumb">			
+			<?php if($relatedprd_count == 1): ?>
+    		<hr>
+    	<?php endif; ?>
+			<div class="content-block row">
+	    	<div class="product-thumb small-5 columns">			
 					<?php if($image_array[0]!='' && file_exists($imagepath)): ?>
 							<?php if($Product->get_product_price_sale($product_id)>0): ?>
-								<img src="<?php bloginfo('template_directory'); ?>/images/sale.png" alt="<?php the_title(); ?>" class="sale_img" />
-							<?php endif; ?>
-					
-							<a href="<?php echo $productlink;?>" class="product_thumb" >
-					 			<img src="<?php echo theme_thumb($image_array[0],139); ?>" title="<?php echo $product_post_title;?>" alt="<?php echo $product_post_title;?>"/>
+								<img src="<?php bloginfo('template_directory'); ?>/images/sale.png" alt="<?php the_title(); ?>" class="sale-image" />
+							<?php endif; ?>						
+							<a href="<?php echo $productlink;?>">
+					 			<img src="<?php echo theme_thumb($image_array[0],103, 143); ?>" title="<?php echo $product_post_title;?>" alt="<?php echo $product_post_title;?>"/>
 				 			</a>
 					<?php endif; ?>
-	    		<br />
+				</div>
+	    	<div class="small-7 columns">
 	      	<a href="<?php echo $productlink;?>"><?php echo $product_post_title;?></a> 
       	</div>
     	</div>
+    	<?php if($relatedprd_count < count($post_array)): ?>
+    		<hr>
+    	<?php endif; ?>
 		<?php endif; ?>
 		<?php
 		if($relatedprd_count==4)
